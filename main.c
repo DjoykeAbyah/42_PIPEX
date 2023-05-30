eff2b90c7e408ec6b9fe54a2b94494cfd19203b6
DjoykeAbyah <crudendjoyke@gmail.com>
2023-05-30 12:45:35 +0200

    start

--- main.c
+++ main.c
@@ -6,9 +6,22 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/24 17:03:21 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/05/25 19:31:02 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/05/30 12:40:31 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include "pipex.h"
 
-int	main(int argc, char **argv, char **envp)
+// int	main(int argc, char **argv, char **envp)
+
+int	main(void)
+{
+	int	fd;
+
+	fd = open("file1", O_RDONLY);
+	while (1)
+	{
+		create a file2 for the output
+	}
+}
--- pipex.h
+++ pipex.h
@@ -6,7 +6,14 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/24 17:03:09 by dreijans      #+#    #+#                 */
-/*   Updated: 2023/05/24 17:03:10 by dreijans      ########   odam.nl         */
+/*   Updated: 2023/05/30 12:05:08 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <fcntl.h>
+
+#endif
